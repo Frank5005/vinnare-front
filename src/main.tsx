f4719118722e294427377a85ae65452cb852128f
Frank5005 <frankill.5005@gmail.com>
2025-05-23 08:24:51 -0500

    Adding import

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import React from 'react'
 import { AuthProvider } from './context/AuthContext'
 import { PurchaseProvider } from './context/purchaseContext'
 
