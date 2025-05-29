62ff65553381d289adb811d3ad7cea675fa6fd7c
Frank5005 <frankill.5005@gmail.com>
2025-05-29 18:44:29 -0500

    Adding cart test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 /*
 const baseURL = import.meta.env.PROD
   ? 'https://5586-3-147-45-32.ngrok-free.app'
   : '/api';
-  */
+*/
 
 const api = axios.create({
   //baseURL: baseURL,
@@ -13,15 +13,13 @@ const api = axios.create({
   withCredentials: true,
 });
 
-
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");  
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    config.headers['ngrok-skip-browser-warning']="true";
-    return config;
-  });
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.headers["ngrok-skip-browser-warning"] = "true";
+  return config;
+});
 
 export default api;
-
