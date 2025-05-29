4f5356b666298f915e6c165c4f675d7120542c5c
valeriapadilla <vpadillahoyos@gmail.com>
2025-05-28 22:16:23 -0500

    updating the test of components

--- src/pages/Unauthorized.tsx
+++ src/pages/Unauthorized.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Unauthorized = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div data-testid="unauthorized-container" className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-6xl font-bold text-red-600 mb-4">Not found</h1>
       <p className="text-2xl text-gray-800 mb-2"> What you were looking for was not found or you do not have permission to access.</p>
       <p className="text-lg text-gray-500">Please check the URL or contact the administrator.</p>
